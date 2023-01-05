d2d68e7bc61ff410ad9a87ede082496b548ed9ec
Eden <94796854+edenbwt@users.noreply.github.com>
2023-01-05 20:46:30 +0100

    Update main_code_for_fly_computer.ino
    add Led, buzzer and the part for rocket recovery

--- main_code_for_fly_computer.ino
+++ main_code_for_fly_computer.ino
@@ -8,6 +8,13 @@
 Adafruit_BMP280 bmp;
 Adafruit_MPU6050 mpu;
 
+//Create objects for each user device
+const int Led_red = 5;
+const int Led_green = 6;
+const int Led_bleu = 7; //for the Rgb Led on the board
+
+const int Buzzer = 8;
+
 const int pyro1Pin = 2; // pin for pyrotechnic channel 1
 const int pyro2Pin = 3; // pin for pyrotechnic channel 2
 
@@ -16,30 +23,45 @@ int pyro2Status = LOW; // status of pyro channel 2 (LOW = off, HIGH = on)
 
 // Create variables to store sensor data
 float temperature, pressure, altitude;
-int16_t ax, ay, az, gx, gy, gz;
+int16_t ax, ay, az, gx, gy, gz; //a is for acceleration read in Gforce, g is for gyroscope axes placement in space
 
 // Create variables for login data
 File dataFile;
 String dataString;
 
 void setup() {
-// Initialize sensors
-if (!bmp.begin()) {
-Serial.println("Error initializing BMP280");
-}
-if (!mpu.begin()) {
-Serial.println("Error initializing MPU6050");
-}
-
-// Initialize pyro channels
+// Initialize pyro channels 
 pinMode(pyro1Pin, OUTPUT);
 pinMode(pyro2Pin, OUTPUT);
 digitalWrite(pyro1Pin, pyro1Status);
 digitalWrite(pyro2Pin, pyro2Status);
 
+// Initialize Led 
+pinMode(Led_red, OUTPUT);
+pinMode(Led_bleu, OUTPUT);
+pinMode(Led_green, OUTPUT);
+
+digitalWrite(Led_bleu, HIGH);
+digitalWrite(Led_red, HIGH);
+digitalWrite(Led_green, HIGH);
+
+// Initialize Buzzer
+pinMode(Buzzer, OUTPUT);
+
+// Initialize sensors
+if (!bmp.begin()) {
+Serial.println("Error initializing BMP280");
+digitalWrite(Led_red, LOW);
+}
+if (!mpu.begin()) {
+Serial.println("Error initializing MPU6050");
+digitalWrite(Led_red, LOW);
+}
+
 // Initialize SD card
 if (!SD.begin(4)) {
 Serial.println("Error initializing SD card");
+digitalWrite(Led_red, LOW);
 }
 
 // Open data file for writing
@@ -60,12 +82,12 @@ dataString = "Temperature: " + String(temperature) + " C\nPressure: " + String(p
 dataFile.println(dataString);
 
 // Check if pyro channels should be activated
-if (/* some condition /) {
+if (ay < -0.5) {
 pyro1Status = HIGH;
 digitalWrite(pyro1Pin, pyro1Status);
 }
-if (/ some condition */) {
-pyro2Status = HIGH;
-digitalWrite(pyro2Pin, pyro2Status);
-}
+    //if (/ some condition */) {
+    //pyro2Status = HIGH;
+    //digitalWrite(pyro2Pin, pyro2Status);
+    //}
 }
